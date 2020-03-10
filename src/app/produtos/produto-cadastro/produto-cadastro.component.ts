c5c3e4e5dc4e039caee6afe9621eadf8a5672f9d
Dirceu R Junior <dirceu.r.junior@gmail.com>
2020-03-10 19:40:02 -0300

    Alteração Cadastro de Produtos

--- src/app/produtos/produto-cadastro/produto-cadastro.component.ts
+++ src/app/produtos/produto-cadastro/produto-cadastro.component.ts
@@ -139,7 +139,7 @@ export class ProdutoCadastroComponent implements OnInit {
    }
 
    carregarIngredientes() {
-      console.log('entrou metodo carregarIngredientes');
+      console.log('entrou metodo carregarIngredientessssssssssss');
       this.filtro.itensPorPagina = 0;
       this.filtro.categoria = 6;
       this.produtoService.pesquisar(this.filtro)
